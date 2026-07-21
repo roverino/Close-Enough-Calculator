1d59f0f8713feab66f0ef7347dfecc913add33d6
roverino <jr.tombolini@gmail.com>
2026-07-21 15:52:03 -0400

    Attempting to undo commits for test purposes 2

--- CloseEnoughCalculator.py
+++ CloseEnoughCalculator.py
@@ -1,3 +1,15 @@
+import re
+
+def main():
+    while (True):
+        expr = input("Enter a mixed number: ") # change test expression
+        if (expr == "q"):
+            break
+        else:
+            fixedExpr = convertToPostfix(expr)
+            print(f"You entered {expr}, which turned into {fixedExpr}") # change test expression
+    
+
 ## TODO input handling
     # length
     # units
@@ -11,15 +23,66 @@
 # TODO error handling (if number inputted has anything besides numerals, ., and /.)
     # accepted formats x, x.x, x/x, x.x/x.x, x.x/x.x
 
-# TODO parse mixed fractions AND parentheses into floats
-    # some sort of recursion is required
-    # if (, then the entire term until the next ) ......that doesnt work for ((4/3)/4)
-    # parse expression
-    # if (, then grab expression. parse smallerExpr should run and eliminate all open brackets that come before a close bracket
-    # parse smallerExpr should parse expression.
-    # parse expression has if (, so else case 
+# TODO low priority try to implement a tree version of evaluating infix instead of postfix
 
 # TODO earlier than the parser, just dont accept mixed fractions or parenths
 
+# TODO sanitize inputs and use ast to assert mathematical expression is valid
+# TODO convert to postfix
+""" RULES FOR INFIX to POSTFIX:
+- extra step for mixed numbers: replace all spaces with + (or rather replace all spaces followed by a digit)
+- remove extra spaces
+- going left to right...
+- numbers go on the output stack
+- when encountering an operator
+    - while operator isnt a left paren AND it has a lesser or equal prescendence
+        - pop the top of the stack
+        - REMEMBER, left operands of equal precedence have GREATER precedence because left to right rule (rule of associativity)
+    - push operator onto operator stack
+- when encountering a left paren
+    - push it onto the stack 
+- when encountering a right paren
+    - pop every operator on the stack until you get to a left paren (while loop)
+        - make sure stack is not empty before hitting a left paren, if so return error "unexpected item in bagging area"
+        - remember you still follow rules of precedence while dealing with parens
+    - pop and destroy the left paren once you hit it
+- when empty, pop all the operators remaining on the output stack
+"""
 
-# TRY converting to postfix, solving via postfix
+def convertToPostfix(expr:str):
+    postfix = ""
+
+    # DESCRIPTION: Converting mixed numbers into postfix friendly expressions
+    # Why not just convert instances of spaces into +'s if you assume no user error?
+        # "3 * 1 1/8" would be incorrect due to order of operations
+    expr = re.sub(
+        r"(\d+)\s+(\d+)\s*/\s*(\d+)",
+        r"(\1+\2/\3)",expr)
+        # Parentheses "capture" an instance of the expression we are currently replacing
+        # Each capture is assigned to 1, 2, 3... etc, and thus can be manipulated as such
+        # re.sub will replace all instances so no need for a loop
+    expr = expr.replace(" ", "")
+    return expr
+
+    operandStack = list()
+    finalExpr = str()
+
+    
+
+    # replace all spaces with addition symbol
+    # NOTE this only works if user input is correct. more robust way would be regex
+    # i think regex would be "\d+ d+\/\d+", and then add a space, 
+    # but doing that might be unncessarily hard for a first version
+    return postfix
+
+# TODO evaluate postfix
+"""Rules for evaluating postfix
+- push numbers onto the stack as they come
+- when encountering an operand, pop the last two numbers and evaluate them them
+    - push the result back onto the stack
+- repeat until only one number remains and you've run through the whole expression
+"""
+
+if __name__ == "__main__":
+    # i think this setup means that this file will act as a library unless run directly
+    main()
